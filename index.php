38d63b5957b8ec2ce7e7894398b3ad9ebf01c969
Sites-ENSCI <sites@ensci.com>
2026-04-22 14:03:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<HTML lang="fr">
+<?php include "./php/meta_header.html";?>
+  <body class="body">
+    <div class="corps_index">
+      <div class="titrage">
+        <img src="./img/symbole-lecture-blanc.svg" alt="symbole &quot;Lecture vidéo&quot;" class="titrage">
+        <div id="titrage_texte">
+          <h1 class="titrage"><B>Vidéothèque</B> complémentaire<br/>au mémoire de Betty LUJAN</h1>
+          <p class="titrage">sous la direction de Lucinda GROUEFF</p>
+        </div>
+      </div>
+      <div class="texte">
+        <p class="texte">
+          Cette vidéothèque réunit une sélection de rushes tournés lors de mes
+          explorations urbaines en Ouzbékistan, aux Émirats arabes unis et à Londres,
+          en début de recherche du mémoire.<br/>
+          &emsp;Ces vidéos, que je montre à l’état brut, ont été un matériau de recherche.
+          La production d’images m’a permis de garder une trace de mes explorations,
+          mais la pratique de la vidéo a également été un outil de
+          pensée pour me questionner sur la ville.<br/>
+          &emsp;Au fil de la lecture du mémoire, le symbole <img src="./img/symbole-lecture-blanc.svg" alt="symbole &quot;Lecture vidéo&quot;" class="texte"> indique que
+          je m’appuie ponctuellement sur un rush disponible dans cette vidéothèque.
+        </p>
+        <a href="./php/Explorer_auto.php" class="texte">Accéder à la vidéothèque</a>
+      </div>
+    </div>
+  </body>
+</HTML>
+<!-- espace cadratin : &emsp -->
